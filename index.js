74ffebce3985b00dcc2f0cb110b8911ed1975623
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-11-21 23:22:45 -0500

    aded index route

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello")
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index', { text: 'AGAIN' })
+})
+
+app.listen(3000)
