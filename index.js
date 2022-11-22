9f2c0fa8f7774ba00f966752da724cfdd430530b
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-11-22 08:42:38 -0500

    set up MVC structure

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index', { text: 'AGAIN' })
+
 })
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
 
 app.listen(3000)
